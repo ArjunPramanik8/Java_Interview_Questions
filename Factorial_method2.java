a38acd0ede0b15958e72db4690f6535696afb40a
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-08-24 23:34:05 +0530

    Factorial_method2

---
+++ Factorial_method2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Factorial_Method2
+{
+    public static int findFactorial(int n)
+    {
+        int f=1;
+        for (int i = 1; i <= n; i++)
+        {
+            f=f*i;
+        }
+        return f;
+    }
+
+    public static void main(String[] args)
+    {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+
+        int factorial= findFactorial(n);
+        System.out.println("Factorial of "+ n + " is "+ factorial );
+
+    }
+}
