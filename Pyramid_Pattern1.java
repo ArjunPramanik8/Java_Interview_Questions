a1a95ba4364a5a38c4a38b0cf82dbc1a15711a4f
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-09-04 22:44:42 +0530

    Pyramid_Pattern1

---
+++ Pyramid_Pattern1.java
@@ -0,0 +1,38 @@
+package Question_Solutions;
+
+import java.util.Scanner;
+
+public class Pyramid_Pattern1
+{
+    public static void main(String[] args)
+    {
+
+        //Get the number of rows
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows needed");
+        int Noofrow = sc.nextInt();
+
+        int rowcount = 1;
+        //Implementing the pyramid logic
+
+        for (int i = Noofrow; i >0 ; i--)
+        {
+
+            //For printing the space before
+            for (int j = 1; j <=i; j++)
+            {
+                System.out.print(" ");
+
+            }
+
+            for (int j = 1; j <=rowcount; j++)
+            {
+                System.out.print(rowcount+" ");
+            }
+            System.out.println();
+            rowcount++;
+        }
+
+    }
+
+}
