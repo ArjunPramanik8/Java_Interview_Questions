762beb64a9b60c3459c309ecc342b9e9ab3e1b71
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-11-11 23:27:44 +0530

    Update Reversing_String.java

--- Reversing_String.java
+++ Reversing_String.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Reversing_String
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+        {
 
         System.out.println("Enter the String to be reversed");
         Scanner sc = new Scanner(System.in);
