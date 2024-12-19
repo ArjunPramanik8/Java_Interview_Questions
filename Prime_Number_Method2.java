8bb231839e39e3222689d58f1833e224445ce1ca
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-12-19 23:10:03 +0530

    Update Prime_Number_Method2.java

--- Prime_Number_Method2.java
+++ Prime_Number_Method2.java
@@ -14,6 +14,7 @@ public class Prime_Number_Method2
         }
         else 
         {
+            
             for (int i = 2; i < n-1; i++) {
                 if (n % i == 0) {
                     c++;
